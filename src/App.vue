7cd5606c36093111222405263d0dd1342bb3891a
Chanon Sidawong <66160057@go.buu.ac.th>
2026-06-14 00:31:19 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Hello from './components/HelloComponent.vue'
+</script>
+
+<template>
+  <div>ขอให้ My order รับฝึกงานครับ</div>
+  <Hello></Hello>
+  <Hello></Hello>
+</template>
+
+<style scoped></style>
