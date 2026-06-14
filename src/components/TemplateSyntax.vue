3ab572b29aebbbf73c35a54e336eb55328c47cbc
Chanon Sidawong <66160057@go.buu.ac.th>
2026-06-14 12:43:52 +0700

    V-bindV2

--- src/components/TemplateSyntax.vue
+++ src/components/TemplateSyntax.vue
@@ -1,3 +1,25 @@
+<script setup>
+var msg = 'Hello Message'
+var rawHtml = '<h1>Hello</h1>'
+var myTd = 'myId1'
+var color = 'green'
+var isDisabled = true
+const objectOfAttrs = {
+  id: 'container',
+  class: 'wrapper',
+  style: 'background-color:green',
+}
+var number = 1
+var ok = true
+</script>
+
 <template>
-  <div>Template Syntax</div>
+  <div>{{ msg }}</div>
+  <div v-html="rawHtml"></div>
+  <div :id="myTd"></div>
+  <font :color="color">งง</font>
+  <button :disabled="isDisabled">isEnabled</button>
+  <div v-bind="objectOfAttrs">สีเขียวนะอันนี้</div>
+  <div>{{ number + 1 }}</div>
+  <div>{{ ok ? 'Yes' : 'No' }}</div>
 </template>
