b3c80ed6c053989bc0b50b61737f8967ff5f9cdb
Chanon Sidawong <66160057@go.buu.ac.th>
2026-06-14 11:57:20 +0700

    templateSyntax

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <script setup>
 import Hello from './components/HelloComponent.vue'
+import TemplateSyntax from './components/TemplateSyntax.vue'
 </script>
 
 <template>
   <div>ขอให้ My order รับฝึกงานครับ</div>
-  <Hello></Hello>
-  <Hello></Hello>
+  <span><Hello></Hello></span>
+  <TemplateSyntax></TemplateSyntax>
 </template>
 
 <style scoped></style>
