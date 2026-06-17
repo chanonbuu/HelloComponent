09cc4f0f83c061e03b1b92d388b8996817893b49
Chanon Sidawong <66160057@go.buu.ac.th>
2026-06-17 20:43:31 +0700

    ProgramHelloMyName

--- src/App.vue
+++ src/App.vue
@@ -3,18 +3,21 @@ import Hello from './components/HelloComponent.vue'
 import TemplateSyntax from './components/TemplateSyntax.vue'
 import ReactivityFundamentals from './components/ReactivityFundamentals.vue'
 import FormInputBindings from './components/FormInputBindings.vue'
+import HelloMyName from './components/HelloMyName.vue'
 </script>
 
 <template>
   <div>ขอให้ My order รับฝึกงานครับ</div>
-  <hr>
+  <hr />
   <span><Hello></Hello></span>
-  <hr>
+  <hr />
   <TemplateSyntax></TemplateSyntax>
-  <hr>
+  <hr />
   <ReactivityFundamentals></ReactivityFundamentals>
-  <hr>
+  <hr />
   <FormInputBindings></FormInputBindings>
+  <hr />
+  <HelloMyName></HelloMyName>
 </template>
 
 <style scoped></style>
---
+++ src/components/HelloMyName.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <input type="text" v-model="name" />
+    <div>Hello {{ name }}</div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+const name = ref('')
+</script>
